7e802c419d177eefe8c5e27420cdb9e645a3cd6d
Divyanshu <sap.divyanshu@gmail.com>
2025-04-08 16:48:22 +0530

    First changes

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("Hello world");
+
+const express = require('express');
+const vendorData = require('./vendors.json');
+var app = express();
+app.listen(3001, ()=> console.log("The server is now running at http://localhost:3001"));
+app.get("/",(req, res) => {
+    // console.log(req);
+    res.send("Helllo, welcome to my server");
+});
+app.get("/vendors",(req, res) => {
+    res.json(vendorData.vendors);
+});
+
+app.get("/vendors/:id",(req, res) =>{
+    // console.log(req.url);
+    // console.log(req.originalUrl);
+    // console.log(req.params);
+    // console.log(req.params.id);
+    // res.send("Hello there");
+    var respData = vendorData.vendors.find((element) =>{
+        return element.id == req.params.id;
+    });
+    res.json(respData);
+})
+// console.log("The server is now running on http://localhost:3001");
