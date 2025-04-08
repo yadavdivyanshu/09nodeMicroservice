ba8c96eac1ba12b79eaf1a78148ffdb8c2bb1d17
Divyanshu <sap.divyanshu@gmail.com>
2025-04-08 17:17:09 +0530

    Fourth changes

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 
 const express = require('express');
 const vendorData = require('./vendors.json');
+this.uuid4 = function() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+  }
 var app = express();
+app.use(express.json());
 app.listen(3001, () => console.log("The server is now running at http://localhost:3001"));
 app.get("/", (req, res) => {
     // console.log(req);
@@ -11,6 +17,12 @@ app.get("/", (req, res) => {
 app.get("/vendors", (req, res) => {
     res.json(vendorData.vendors);
 });
+
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+  }
 
 app.get("/vendors/:id", (req, res) => {
     // console.log(req.url);
@@ -30,5 +42,11 @@ app.get("/vendors/:id", (req, res) => {
     typeof (respData) != 'undefined' ? res.json(respData) : res.json({
         "message": "No vendor found"
     });
+});
+
+app.post("/vendors",(req,res) =>{
+    var payload = req.body;
+    payload.id = this.uuid4();
+    res.json(payload);
 });
 // console.log("The server is now running on http://localhost:3001");
