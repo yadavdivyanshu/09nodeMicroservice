d405af69e5f5d72bd4dcc1208abbbe7222f37e21
Divyanshu <sap.divyanshu@gmail.com>
2025-06-08 13:18:39 +0530

    cf push reeady old one

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // console.log("Hello world");
 
+// const express = require('express');
 const express = require('express');
 const vendorData = require('./vendors.json');
-this.uuid4 = function() {
+
+this.uuid4 = function () {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-      (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
     );
-  }
+};
 var app = express();
 app.use(express.json());
-app.listen(3001, () => console.log("The server is now running at http://localhost:3001"));
+app.use("/",express.static("webapp/"));
+const PORT = process.env.PORT || 3000;
+// app.listen(3001, () => console.log("The server is now running at http://localhost:3001"));
+app.listen(PORT, () => console.log("The server is now running at http://localhost:" + PORT));
 app.get("/", (req, res) => {
     // console.log(req);
     res.send("Helllo, welcome to my server");
@@ -18,11 +23,11 @@ app.get("/vendors", (req, res) => {
     res.json(vendorData.vendors);
 });
 
-function uuidv4() {
-    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-      (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
-    );
-  }
+// function uuidv4() {
+//     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+//       (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+//     );
+//   }
 
 app.get("/vendors/:id", (req, res) => {
     // console.log(req.url);
@@ -44,9 +49,13 @@ app.get("/vendors/:id", (req, res) => {
     });
 });
 
-app.post("/vendors",(req,res) =>{
+app.post("/vendors", (req, res) => {
     var payload = req.body;
     payload.id = this.uuid4();
     res.json(payload);
+});
+
+app.get("/index.html", (req, res) => {
+    res.sendFile(__dirname + "/webapp/index.html");
 });
 // console.log("The server is now running on http://localhost:3001");
